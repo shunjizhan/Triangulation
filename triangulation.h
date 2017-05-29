dfceac0205c6460e4189cfe079d2d8d823e83686
shunji zhan <shunji.zhan@procore.com>
2017-05-28 19:41:42 -0700

    can compute area based on three points

--- triangulation.h
+++ triangulation.h
@@ -6,14 +6,14 @@
 using namespace std;
 
 struct Point {
-  float x, y, z;
+  double x, y, z;
   Point() {
     this->x = 0;
     this->y = 0;
     this->z = 0;
   }
 
-  Point(float x, float y, float z) {
+  Point(double x, double y, double z) {
     this->x = x;
     this->y = y;
     this->z = z;
@@ -30,7 +30,7 @@ struct Plane {
   Plane() {
   }
 
-  void addPoint(float x, float y, float z) {
+  void addPoint(double x, double y, double z) {
     allPoints.push_back(Point(x, y, z));
   }
 
@@ -50,7 +50,7 @@ public:
     lower = Plane();
   }
 
-  void addPoint(float x, float y, float z) {
+  void addPoint(double x, double y, double z) {
     if (z == 1) {
       upper.addPoint(x, y, z);
     } else {
@@ -61,9 +61,36 @@ public:
   void print() {
     upper.print();
     lower.print();
+
+    cout << "-------" << endl;
+    Point p1, p2, p3;
+    p1 = upper.allPoints[0];
+    p2 = upper.allPoints[1];
+    p3 = lower.allPoints[0];
+    p1.print();
+    p2.print();
+    p3.print();
+    double area = getArea(p1, p2, p3);
+    cout << area << endl;
+  }
+
+  double getDistance(Point p1, Point p2) {
+    double d1 = abs(p1.x - p2.x);
+    double d2 = abs(p1.y - p2.y);
+    double d3 = abs(p1.z - p2.z);
+    return sqrt((d1 * d1) + (d2 * d2) + (d3 * d3));
+  }
+
+  double getArea(Point p1, Point p2, Point p3) {
+    double a = getDistance(p1, p2);
+    double b = getDistance(p2, p3);
+    double c = getDistance(p1, p3);
+    double s = (a + b + c) / 2;
+    return sqrt(s * (s - a) * (s - b) * (s - c));
   }
 
 };
+
 
 
 
